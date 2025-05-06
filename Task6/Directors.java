13aa78f0aabaab7d071ea424fcc35b2e07e8b851
Fredrik Fordelsen <fredrikf2000@gmail.com>
2025-05-06 16:22:04 +0200

    directors init

--- Task6/Directors.java
+++ Task6/Directors.java
@@ -1,5 +1,16 @@
 package Task6;
+/*
+• Task 6.9: Additional Field in the Directors Subclass (2 points)
+    • The Directors subclass should contain an additional field that stores the name of the
+    last movie directed.
 
+    • Task 6.10: Polymorphic Print Methods in Directors (4 points)
+    • Use polymorphism to create two methods in the Directors class to print information
+    about the directors:
+    • One method should print only the first name and last name.
+    • The other method should print all available information, including the last
+    movie directed.
+ */
 public class Directors extends CompanyEmployee {
     
 }
