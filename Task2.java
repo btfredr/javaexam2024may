77d57f1c8ed936977d8a57126363acd32208e8e0
Fredrik Fordelsen <fredrikf2000@gmail.com>
2025-05-06 13:01:15 +0200

    prompting user for input

--- Task2.java
+++ Task2.java
@@ -16,5 +16,6 @@ import java.util.Scanner;
 public class Task2 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        System.out.print("Please enter the number of rows: ");
     }
 }
