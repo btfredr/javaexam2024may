4ec84ec08de33877b96b9a656cbcef97a2dd3716
Fredrik Fordelsen <fredrikf2000@gmail.com>
2025-05-06 16:13:44 +0200

    Actors init

--- Task6/Actors.java
+++ Task6/Actors.java
@@ -1,5 +1,25 @@
 package Task6;
 
+/*
+• Task 6.5: Additional Fields in the Actors Subclass (1 point)
+    • The Actors subclass should include:
+    • A field that holds the main genre the actor typically performs in (e.g., drama,
+    comedy).
+    • A field that indicates whether the actor is currently available for new roles.
+
+    • Task 6.6: Constructor for Actors (3 points)
+    • Actors should have an additional constructor that updates the two new fields in
+    addition to the constructor from the superclass.
+
+    • Task 6.7: Methods in Actors (3 points)
+    • Add methods getGenre and isAvailable that read and return the values of the fields
+    genre and available, respectively. 
+
+    • Task 6.8: Override Print Method in Actors (3 points)
+    • Override the print method from the superclass to create a method in the Actors class
+    that prints information from all available fields.
+ */
+
 public class Actors extends CompanyEmployee {
     
 }
