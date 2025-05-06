0010464c1350c3e96ab84ff16eb7aecd316fb995
Fredrik Fordelsen <fredrikf2000@gmail.com>
2025-05-06 16:08:33 +0200

    added method constructors

--- Task6/CompanyEmployee.java
+++ Task6/CompanyEmployee.java
@@ -54,12 +54,66 @@ package Task6;
     • Task 6.11: Test Class with Main Method (5 points)
     • Create a simple Test class with a main method, that will create instances of one
     Actor and one Director and print details from both.
-    
+
     Pay attention to the use of access modifiers and the readability of the code. During the assessment
     of this task, special attention will be focused on the ability to implement fundamental principles of
     object-oriented programming: inheritance, encapsulation, and polymorphism.
  */
 
 public class CompanyEmployee {
+    private int id;
+    private String firstName;
+    private String lastName;
+    private int age;
+
+    // Getters and setters
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    // Default constructors
+    
+    public CompanyEmployee(int id, String firstName, String lastName) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public CompanyEmployee(int id, String firstName, String lastName, int age) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+    }
+
     
 }
