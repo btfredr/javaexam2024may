a4151dfd8d439f97a2b7f15cea586e400e002167
Fredrik Fordelsen <fredrikf2000@gmail.com>
2025-05-06 13:42:23 +0200

    Task5 init

---
+++ Task5.java
@@ -0,0 +1,19 @@
+/*
+    Task 5. (15 points)
+
+    Write a Java program that analyzes the input string using the method:
+
+    public void analyzeString(String input){}
+    The analysis should include: the number of uppercase letters, the number of lowercase letters, the
+    string length, and the number of spaces.
+    
+    E.g.: if the input string is: “Madam, I’m Adam”, the method should print:
+    • The string contains 2 uppercase letters!
+    • The string contains 7 lowercase letters!
+    • The string is 16 characters long!
+    • The string contains 2 spaces!
+ */
+
+public class Task5 {
+    
+}
