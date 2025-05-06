b7aa4d0f4c79c009f7a390747b13732f7ce54919
Fredrik Fordelsen <fredrikf2000@gmail.com>
2025-05-06 12:52:20 +0200

    task 1 init

---
+++ Task1.java
@@ -0,0 +1,15 @@
+/*
+    Task 1. (10 points)
+
+    Write a method that takes a double value (input) as an argument and returns the biggest odd
+    integer number smaller than the input number. For example:
+    For input = 25.2, the method returns 25.
+    For input = 7.0, the method returns 5.
+    For input = -5.75, the method returns -7.
+ */
+
+
+public class Task1 {
+
+    
+}
