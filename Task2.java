709316eed54e561910e35786cbd32a2a73b310a2
Fredrik Fordelsen <fredrikf2000@gmail.com>
2025-05-06 13:00:00 +0200

    task 2 init

---
+++ Task2.java
@@ -0,0 +1,16 @@
+/*
+    Task 2. (15 points)
+    
+    Write a Java program that uses two for loops to print out a pyramid of numbers. The pyramid
+    should have a number of rows equal to the height specified by the user. Each row should display
+    incrementing numbers starting from 1. For example, if the user specifies a height of 4, the pyramid
+    printed should look like this:
+    1
+    1 2
+    1 2 3
+    1 2 3 4
+ */
+
+public class Task2 {
+    
+}
